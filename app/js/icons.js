d32084293209031d508f7ef9d9af4258d1b6b469
Sofya <48826470+SofyaPim@users.noreply.github.com>
2021-01-20 18:15:22 +0300

    toucheEvent added

--- app/js/icons.js
+++ app/js/icons.js
@@ -6,13 +6,24 @@ elem.forEach(item => {
     item.addEventListener("mouseover", function changeColor() {
         this.classList.add('filter');
     }, true);
+
     item.addEventListener("pointerover", function changeColor() {
         this.classList.add('filter');
 
     }, true);
+   
+    item.addEventListener( "touchstart", function changeColor() {
+        this.classList.add('filter');
+
+    }, true);
+    item.addEventListener("touchend", function changeColor() {
+        this.classList.remove('filter');
+    }, true);
+
     item.addEventListener("mouseout", function changeColor() {
         this.classList.remove('filter');
     }, true);
+
     item.addEventListener("pointerout", function changeColor() {
         this.classList.remove('filter');
 
