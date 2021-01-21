e7c199e99aa1102d17059785e076d5043b882ac0
Sofya <48826470+SofyaPim@users.noreply.github.com>
2021-01-21 15:13:04 +0300

    refactoring code

--- app/js/icons.js
+++ app/js/icons.js
@@ -2,32 +2,33 @@ let elem = document.querySelectorAll(".filtered");
 
 
 elem.forEach(item => {
+    item.classList.add('filter');
 
-    item.addEventListener("mouseover", function changeColor() {
-        this.classList.add('filter');
-    }, true);
+    // item.addEventListener("mouseover", function changeColor() {
+    //     this.classList.add('filter');
+    // }, true);
 
-    item.addEventListener("pointerover", function changeColor() {
-        this.classList.add('filter');
+    // item.addEventListener("pointerover", function changeColor() {
+    //     this.classList.add('filter');
 
-    }, true);
+    // }, true);
    
-    item.addEventListener( "touchstart", function changeColor() {
-        this.classList.add('filter');
+    // item.addEventListener( "touchstart", function changeColor() {
+    //     this.classList.add('filter');
 
-    }, true);
-    item.addEventListener("touchend", function changeColor() {
-        this.classList.remove('filter');
-    }, true);
+    // }, true);
+    // item.addEventListener("touchend", function changeColor() {
+    //     this.classList.remove('filter');
+    // }, true);
 
-    item.addEventListener("mouseout", function changeColor() {
-        this.classList.remove('filter');
-    }, true);
+    // item.addEventListener("mouseout", function changeColor() {
+    //     this.classList.remove('filter');
+    // }, true);
 
-    item.addEventListener("pointerout", function changeColor() {
-        this.classList.remove('filter');
+    // item.addEventListener("pointerout", function changeColor() {
+    //     this.classList.remove('filter');
 
-    }, true);
+    // }, true);
 
 
 
